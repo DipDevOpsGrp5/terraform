ee83347b3e1739fd4b04e26e393cf1980f8fd9f1
Hector Rojas <hrojasb@gmail.com>
2022-04-22 22:31:35 -0400

    Correcciones

--- aks.tf
+++ aks.tf
@@ -3,13 +3,13 @@ resource "azurerm_kubernetes_cluster" "kubernetescluster" {
     location = azurerm_resource_group.ggrupo5.location
     resource_group_name = azurerm_resource_group.ggrupo5.name
     dns_prefix = "aksl"
-    kubernetes_version = "1.18.14"
+    kubernetes_version = "1.23.5"
 
     default_node_pool {
         name = "default"
         node_count = 1
         vm_size = "Standard_D2_v2"
-        vnet_subnet_id = azurerm_subnet.subnet.id
+        vnet_subnet_id = azurerm_subnet.subnetaks.id
         enable_auto_scaling = true
         max_count = 2
         min_count = 1
@@ -18,11 +18,9 @@ resource "azurerm_kubernetes_cluster" "kubernetescluster" {
     client_id = "hector.rojas@euroamerica.cl"
     client_secret = "miclave"
     }
-
     network_profile {
       network_plugin = "azure"
       network_policy = "azure"
-
     }
 #    role_based_access_control {
 #        azureazure_active_directory {
--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -2,28 +2,21 @@ resource "azurerm_resource_group" "ggrupo5" {
   name     = "ggrupo5-resources"
   location = "West Europe"
 }
-
 resource "azurerm_network_security_group" "sggrupo5" {
   name                = "sggrupo5-security-group"
   location            = azurerm_resource_group.ggrupo5.location
   resource_group_name = azurerm_resource_group.ggrupo5.name
 }
-
 resource "azurerm_virtual_network" "vngrupo5" {
   name                = "grupo5-network"
   location            = azurerm_resource_group.ggrupo5.location
   resource_group_name = azurerm_resource_group.ggrupo5.name
-  address_space       = ["10.0.0.0/16"]
+  address_space       = ["10.0.0.0/25"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
 }
-
-resource "azurerm_subnet" "subnet" {
-    name = "internal"
+resource "azurerm_subnet" "subnetaks" {
+    name = "aksnet"
     resource_group_name = azurerm_resource_group.ggrupo5.name
     virtual_network_name = azurerm_virtual_network.vngrupo5.name
-    address_prefixes = ["12.0.0.0/20"]
-
-  #tags = {
-  #  environment = "Production-Grupo5"
-  #}
+    address_prefixes = ["10.0.0.32/27"]
 }
