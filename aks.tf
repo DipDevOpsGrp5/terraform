4efeeae2b16a07c142363dc6e69ffdd59c8a2fff
Hector Rojas <hrojasb@gmail.com>
2022-04-22 21:52:38 -0400

    Correcciones

--- aks.tf
+++ aks.tf
@@ -1,7 +1,7 @@
 resource "azurerm_kubernetes_cluster" "kubernetescluster" {
     name =  "aksgrupo5"
-    location = azurerm_resource_group.resourcegroup.location
-    resource_group_name = azurerm_resource_group.resourcegroup.name
+    location = azurerm_resource_group.ggrupo5.location
+    resource_group_name = azurerm_resource_group.ggrupo5.name
     dns_prefix = "aksl"
     kubernetes_version = "1.18.14"
 
@@ -9,14 +9,14 @@ resource "azurerm_kubernetes_cluster" "kubernetescluster" {
         name = "default"
         node_count = 1
         vm_size = "Standard_D2_v2"
-        vnet_subnet_id = azurerm_subnet.internal.id
+        vnet_subnet_id = azurerm_subnet.subnet.id
         enable_auto_scaling = true
         max_count = 2
         min_count = 1
     }
     service_principal {
-    client_id = ""
-    clent_secret = ""
+    client_id = "hector.rojas@euroamerica.cl"
+    client_secret = "miclave"
     }
 
     network_profile {
@@ -24,15 +24,13 @@ resource "azurerm_kubernetes_cluster" "kubernetescluster" {
       network_policy = "azure"
 
     }
-    role_based_access_control {
-        azureazure_active_directory {
-            client_app_id = ""
-            server_app_id = ""
-            server_app_secret = ""
-            tenant_id = ""
-
-          
-        } 
-    }
+#    role_based_access_control {
+#        azureazure_active_directory {
+#            client_app_id = ""
+#            server_app_id = ""
+#            server_app_secret = ""
+#            tenant_id = ""         
+#        } 
+#    }
 }
  
--- virtualnetwork.tf
+++ virtualnetwork.tf
@@ -9,9 +9,9 @@ resource "azurerm_network_security_group" "sggrupo5" {
   resource_group_name = azurerm_resource_group.ggrupo5.name
 }
 
-resource "azurerm_virtual_network" "grupo5" {
+resource "azurerm_virtual_network" "vngrupo5" {
   name                = "grupo5-network"
-  location            = azurerm_resource_group.grupo5.location
+  location            = azurerm_resource_group.ggrupo5.location
   resource_group_name = azurerm_resource_group.ggrupo5.name
   address_space       = ["10.0.0.0/16"]
   dns_servers         = ["10.0.0.4", "10.0.0.5"]
@@ -20,10 +20,10 @@ resource "azurerm_virtual_network" "grupo5" {
 resource "azurerm_subnet" "subnet" {
     name = "internal"
     resource_group_name = azurerm_resource_group.ggrupo5.name
-    virtual_network_name = azurerm_resource_network.grupo5.name
+    virtual_network_name = azurerm_virtual_network.vngrupo5.name
     address_prefixes = ["12.0.0.0/20"]
 
-  tags = {
-    environment = "Production-Grupo5"
-  }
+  #tags = {
+  #  environment = "Production-Grupo5"
+  #}
 }
